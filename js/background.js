1b254786cf813e7ca7a8610cb5d331d8a94cd388
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-05 19:21:37 -0500

    began work on background

--- js/background.js
+++ js/background.js
@@ -2,5 +2,12 @@
 function drawBackground() {
     // Example Code follows.
     // Please delete and replace with your own code.
-    background(0, 0, 200);
+    background(0,0,0);
+     
+
+
+    fill (0,0,255);
+    ellipse(0,400,75,75);
+
+
 }
