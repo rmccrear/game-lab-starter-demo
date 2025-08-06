40acd4bf55499a67d2cddb75cf47b646c6f1d78a
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-05 19:08:49 -0500

    Deleted starter code we were instructed to delete.

--- js/background.js
+++ js/background.js
@@ -3,6 +3,4 @@ function drawBackground() {
     // Example Code follows.
     // Please delete and replace with your own code.
     background(220, 0, 0);
-    fill("yellow");
-    rect(0, 300, 400, 400);
 }
