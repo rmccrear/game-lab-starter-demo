f3a7c3c382e651b679402e4af020fb3acb78aef8
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-08-06 17:15:07 -0700

    finish merge

--- js/variables.js
+++ js/variables.js
@@ -8,9 +8,6 @@ var platformAnimation;
 // sprite variables
 var player;
 var star;
-<<<<<<< HEAD
 var score = 0;
-=======
 var platform1;
 var platform2;
->>>>>>> 307578cd38776f23f4b3f4717dbc9377d85219c3
